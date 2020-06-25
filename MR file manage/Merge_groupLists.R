23b03d77f5221fc5a3bc5615f7f79de34d20b5d9
gfraga <gorkafraga@gmail.com>
2020-06-25 10:13:15 +0200

    Create Merge_groupLists.R

---
+++ MR file manage/Merge_groupLists.R
@@ -0,0 +1,130 @@
+rm(list=ls(all=TRUE)) # !
+
+  
+GroupT1T2_LN_V <- c("VP_001","VP_004","VP_006","VP_007","VP_015","VP_018","VP_019","VP_022","VP_026","VP_027","VP_030","VP_033","VP_036","VP_040","VP_045","VP_046","VP_054")
+
+GroupT1_LN_V <- c("VP_001","VP_004","VP_005","VP_006","VP_007","VP_008","VP_012","VP_015","VP_018","VP_019","VP_020","VP_021","VP_022","VP_023",
+                  "VP_026","VP_027","VP_030","VP_031","VP_032","VP_033","VP_036","VP_037","VP_038","VP_039","VP_040","VP_045","VP_046",
+                  "VP_051","VP_053","VP_054","VP_060")
+
+GroupT1_LD_V <-  c("VP_001","VP_004","VP_005","VP_006","VP_007","VP_008","VP_010","VP_012","VP_015","VP_018","VP_019","VP_020","VP_022","VP_023","VP_027","VP_030","VP_033","VP_036","VP_038","VP_040","VP_045","VP_054","VP_060")
+GroupT1_ND_V <-  c("VP_001","VP_004","VP_005","VP_006","VP_007","VP_008","VP_012","VP_015","VP_018","VP_019","VP_020","VP_022","VP_023","VP_027","VP_030","VP_033","VP_036","VP_038","VP_040","VP_045","VP_047","VP_054","VP_060")
+
+
+GroupT2vp2721out <- c("VP_001","VP_002","VP_004","VP_005","VP_006","VP_007","VP_010","VP_015","VP_018","VP_019","VP_022","VP_026",
+                      "VP_030","VP_031","VP_033","VP_035","VP_036","VP_038","VP_040","VP_042","VP_044","VP_045","VP_046","VP_047",
+                      "VP_054","VP_061","VP_067","VP_074","VP_076","VP_077","VP_078","VP_079","VP_080")
+
+
+GroupT2_LN_V <- c("VP_001","VP_002","VP_004","VP_005","VP_006","VP_007","VP_010","VP_015","VP_018","VP_019","VP_021","VP_022","VP_026","VP_027",
+                  "VP_030","VP_031","VP_033","VP_035","VP_036","VP_038","VP_040","VP_042","VP_044","VP_045","VP_046","VP_047",
+                  "VP_054","VP_061","VP_067","VP_074","VP_076","VP_077","VP_078","VP_079","VP_080")
+
+GroupT2_LD_V <- c("VP_001","VP_002","VP_004","VP_005","VP_006","VP_015","VP_019","VP_021","VP_022","VP_026","VP_030","VP_031","VP_033","VP_035","VP_036","VP_038","VP_040","VP_044",
+                  "VP_061","VP_067","VP_076","VP_077","VP_078","VP_079")
+GroupT2_ND_V <- c("VP_001","VP_002","VP_004","VP_005","VP_006","VP_015","VP_019","VP_021","VP_022","VP_026","VP_030","VP_031","VP_033","VP_035","VP_036","VP_038","VP_040","VP_044",
+                  "VP_061","VP_067","VP_076","VP_077","VP_078","VP_079")
+
+
+GroupT3_all <- c("VP_001","VP_002","VP_004","VP_005","VP_006","VP_007","VP_010","VP_015","VP_018","VP_019","VP_021","VP_022","VP_023","VP_026","VP_027",
+                 "VP_030","VP_031","VP_033","VP_035","VP_036","VP_038","VP_040","VP_042","VP_045","VP_046","VP_047",
+                 "VP_054","VP_061","VP_067","VP_074","VP_076","VP_077","VP_078","VP_079","VP_080","VP_084")
+
+
+GroupT3_trim <- c("VP_001","VP_002","VP_004","VP_005","VP_006","VP_007","VP_010","VP_015","VP_018","VP_019","VP_021","VP_022","VP_026","VP_027",
+                  "VP_030","VP_031","VP_033","VP_035","VP_036","VP_038","VP_040","VP_042","VP_046","VP_047",
+                  "VP_054","VP_061","VP_067","VP_074","VP_076","VP_077","VP_078","VP_080","VP_084")
+
+
+GroupT3_LN_V <- c("VP_001","VP_002","VP_004","VP_005","VP_006","VP_007","VP_010","VP_015","VP_018","VP_019","VP_021","VP_022","VP_026","VP_027",
+                  "VP_030","VP_031","VP_033","VP_035","VP_036","VP_038","VP_040","VP_042","VP_045","VP_046","VP_047",
+                  "VP_054","VP_061","VP_067","VP_074","VP_076","VP_077","VP_078","VP_079","VP_080","VP_084")
+
+GroupT3_LD_V <- c("VP_001","VP_005","VP_007","VP_008","VP_010","VP_015","VP_018","VP_019","VP_021","VP_022","VP_026","VP_027",
+                  "VP_030","VP_031","VP_033","VP_035","VP_036","VP_038","VP_040","VP_042","VP_044","VP_045",
+                  "VP_054","VP_061","VP_067","VP_074","VP_077","VP_078","VP_079","VP_080")
+
+
+GroupT3_ND_V <- c("VP_001","VP_005","VP_007","VP_010","VP_015","VP_018","VP_019","VP_021","VP_022","VP_026","VP_027",
+                  "VP_030","VP_031","VP_032","VP_033","VP_035","VP_036","VP_038", "VP_040","VP_042","VP_045","VP_054","VP_061","VP_067","VP_074","VP_077","VP_078","VP_079","VP_080")
+
+# GroupT2_LN_V <- c("VP_001","VP_002","VP_004","VP_005","VP_006","VP_007","VP_010","VP_015","VP_018","VP_019","VP_021","VP_022","VP_026",
+#                   "VP_027","VP_030","VP_031","VP_033","VP_035","VP_036","VP_038","VP_040","VP_042","VP_044","VP_045","VP_046","VP_047",
+#                   "VP_054","VP_061","VP_067","VP_074","VP_076","VP_077","VP_078","VP_079","VP_080")
+
+GroupT5_LN_V <- c("VP_001","VP_002","VP_006","VP_007","VP_010","VP_015","VP_018","VP_019","VP_022","VP_023","VP_026",
+                  "VP_027","VP_030","VP_031","VP_033","VP_035","VP_036","VP_038","VP_040","VP_042","VP_046","VP_053","VP_054","VP_061",
+                  "VP_067","VP_074","VP_076","VP_077","VP_078","VP_079","VP_080","VP_084")
+
+GroupT5_LD_V <- c("VP_001","VP_002","VP_006","VP_007","VP_015","VP_018","VP_019","VP_022","VP_026","VP_027","VP_030","VP_031","VP_036","VP_038","VP_040","VP_046","VP_053","VP_054","VP_058","VP_061","VP_067","VP_074","VP_076","VP_078","VP_079","VP_084")
+
+
+
+GroupT5_ND_V <- c("VP_001","VP_002","VP_004","VP_006","VP_015","VP_018","VP_019","VP_022","VP_027","VP_030","VP_031","VP_036","VP_038","VP_039","VP_040","VP_046","VP_053","VP_054","VP_061","VP_067","VP_074","VP_076","VP_078","VP_079","VP_084")
+#VP007 and VP026 were excluded from GroupT5_ND_V
+
+
+GroupT6_LN_V <- c("VP_001","VP_002","VP_006","VP_007","VP_010","VP_012","VP_015","VP_018","VP_019","VP_021","VP_025","VP_030","VP_031","VP_033","VP_036",
+                  "VP_038","VP_039","VP_040","VP_042","VP_046","VP_061","VP_067","VP_080","VP_084","VP_101","VP_102","VP_103","VP_104","VP_105","VP_107",
+                  "VP_108","VP_109","VP_110","VP_111","VP_112","VP_115","VP_116","VP_117","VP_118","VP_119","VP_120","VP_121")
+
+GroupT6_LD_V <- c("VP_001","VP_002","VP_006","VP_007","VP_010","VP_012","VP_015","VP_018","VP_019","VP_021","VP_025","VP_030","VP_031","VP_033","VP_036","VP_038",
+                  "VP_039","VP_040","VP_042","VP_046","VP_061","VP_067","VP_080","VP_084","VP_101","VP_102","VP_103","VP_104","VP_105","VP_107","VP_108","VP_109",
+                  "VP_110","VP_111","VP_112","VP_115","VP_116","VP_117","VP_118","VP_119","VP_120","VP_121")
+
+GroupT6_ND_V <- c("VP_001","VP_002","VP_006","VP_007","VP_010","VP_012","VP_015","VP_018","VP_019","VP_021","VP_025","VP_030","VP_031","VP_033","VP_036","VP_038",
+                  "VP_039","VP_040","VP_042","VP_046","VP_061","VP_067","VP_080","VP_084","VP_101","VP_102","VP_103","VP_104","VP_105","VP_107","VP_108","VP_109",
+                  "VP_110","VP_111","VP_112","VP_115","VP_116","VP_117","VP_118","VP_119","VP_120","VP_121")
+
+JasmineSelection <- c("VP_001","VP_006","VP_007", "VP_015","VP_018","VP_019","VP_030","VP_031",
+                      "VP_033","VP_036","VP_038","VP_040", "VP_046")
+
+
+GroupT1T2T5T6_LN_V <- c("VP_001", "VP_006","VP_007","VP_015","VP_018","VP_019","VP_030","VP_033","VP_036","VP_040","VP_042","VP_046")
+
+
+
+
+
+
+# -----------------------------------------------------------------------------------------------
+G <- data.frame(matrix(ncol = 21, nrow = 50))
+colnames(G) <- c("GroupT1T2_LN_V","GroupT1_LN_V","GroupT1_LD_V","GroupT1_ND_V", 
+                 "GroupT2_LN_V","GroupT2_LD_V","GroupT2_ND_V",
+                 "GroupT3_LN_V","GroupT3_LD_V","GroupT3_ND_V","GroupT3_trim",
+                 "GroupT5_LN_V","GroupT5_LD_V","GroupT5_ND_V",
+                 "GroupT6_LN_V","GroupT6_LD_V","GroupT6_ND_V",
+                 "JasmineSelection","GroupT1T2T5T6_LN_V")
+
+
+G$GroupT1T2_LN_V[1:length(GroupT1T2_LN_V)] <- GroupT1T2_LN_V
+G$GroupT1_LN_V[1:length(GroupT1_LN_V)] <- GroupT1_LN_V
+G$GroupT1_LD_V[1:length(GroupT1_LD_V)] <- GroupT1_LD_V
+G$GroupT1_ND_V[1:length(GroupT1_ND_V)] <- GroupT1_ND_V
+
+
+G$GroupT2_LN_V[1:length(GroupT2_LN_V)] <- GroupT2_LN_V
+G$GroupT2_LD_V[1:length(GroupT2_LD_V)] <- GroupT2_LD_V
+G$GroupT2_ND_V[1:length(GroupT2_ND_V)] <- GroupT2_ND_V
+
+
+G$GroupT3_LN_V[1:length(GroupT3_LN_V)] <- GroupT3_LN_V
+G$GroupT3_LD_V[1:length(GroupT3_LD_V)] <- GroupT3_LD_V
+G$GroupT3_ND_V[1:length(GroupT3_ND_V)] <- GroupT3_ND_V
+G$GroupT3_trim[1:length(GroupT3_trim)] <- GroupT3_trim
+
+G$GroupT5_LN_V[1:length(GroupT5_LN_V)] <- GroupT5_LN_V
+G$GroupT5_LD_V[1:length(GroupT5_LD_V)] <- GroupT5_LD_V
+G$GroupT5_ND_V[1:length(GroupT5_ND_V)] <- GroupT5_ND_V
+
+G$GroupT6_LN_V[1:length(GroupT6_LN_V)] <- GroupT6_LN_V
+G$GroupT6_LD_V[1:length(GroupT6_LD_V)] <- GroupT6_LD_V
+G$GroupT6_ND_V[1:length(GroupT6_ND_V)] <- GroupT6_ND_V
+
+G$JasmineSelection[1:length(JasmineSelection)] <- JasmineSelection
+G$GroupT1T2T5T6_LN_V[1:length(GroupT1T2T5T6_LN_V)] <- GroupT1T2T5T6_LN_V
+
+
+G[is.na(G)] <-''
+
+xlsx::write.xlsx(G,'Groups_subsamples_lists.xlsx',row.names = FALSE)
